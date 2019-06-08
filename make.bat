c4296d627fc241844083b82bb9ac703f5ceb341c
marlin <1686157273@qq.com>
2019-06-08 11:01:13 +0800

    all done

--- make.bat
+++
@@ -1,3 +0,0 @@
-bison -d mc.y
-flex  mc.l
-gcc -o mc.exe mc.tab.c lex.yy.c syntax.c semantic.c -lfl
--- ref/hello.c
+++
@@ -1,8 +0,0 @@
-int a=10;
-
-int main(){
-    int b=20;
-    int c;
-    c=a+b;
-    return 0;
-}
